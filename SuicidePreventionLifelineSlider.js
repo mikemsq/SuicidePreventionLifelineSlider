d656186f5523dda9f53b13971e0ba76d6e6e8f26
Michael Vashevko <mvashevko@gmail.com>
2014-04-28 22:11:17 -0400

    added left/right align

--- SuicidePreventionLifelineSlider.js
+++ SuicidePreventionLifelineSlider.js
@@ -39,14 +39,29 @@ if (typeof jQuery === "undefined") {
         $(document).ready(function() {
 			var panel = '<div class="panel ' + options.panelClass + '"><a href="http://www.suicidepreventionlifeline.org" target="_blank">' + options.panelContent + '</a></div>';
 			var toggle = '<div class="toggle ' + options.toggleClass + '" title="Get Help | Click to Expand">' + options.toggleContent + '</div>';
-			var $slider = $('<div class="slider">' + panel + toggle + '</div>');
+			var $slider = $('<div class="slider">' + panel + '<div>' + toggle + '</div></div>');
 			$('body').append($slider);
 			$slider.css('position', 'fixed');
 			$slider.css('top', '0');
+
+			if (options.align == 'left') {
+				$slider.css('left', '10px');
+			} else if (options.align == 'right') {
+				$slider.css('right', '10px');
+				$slider.find('> div').css('text-align', 'right');
+			} else if (options.align == 'center') {
+				//$slider.css('margin-left', 'auto');
+				//$slider.css('margin-right', 'auto');
+				$slider.find('> div').css('margin-left', 'auto');
+				$slider.find('> div').css('margin-right', 'auto');
+				$slider.css('left', '50%');
+				$slider.css('margin-left', '-' + $slider.width()/2 + 'px');
+				//$slider.find('> div').css('left', '50%');
+			}
 			var $panel = $slider.find('> .panel');
 			$panel.hide();
 
-			var $toggle = $slider.find('> .toggle');
+			var $toggle = $slider.find('> div > .toggle');
 			$toggle.click(function () {
 				$panel.slideToggle();
 			});
